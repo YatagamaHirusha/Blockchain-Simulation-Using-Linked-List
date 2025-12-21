58f988e38ca783dc307b7942e907271d237ed785
hirushayatagama <hirushayatagama@gmail.com>
2025-12-21 23:57:20 +0530

    add validation fucntion to check the chain

--- Blockchain.py
+++ Blockchain.py
@@ -19,3 +19,16 @@ class Blockchain:
         new_block = Block(new_index, new_data, previous_block.hash)
 
         self.chain.append(new_block)
+
+    def is_chain_valid(self):
+        for i in range(1, len(self.chain)):
+            current_block = self.chain[i]
+            previous_block = self.chain[i - 1]
+
+            if current_block.hash != current_block.calculate_hash():
+                return False
+
+            if current_block.previous_hash != previous_block.hash:
+                return False
+
+        return True
