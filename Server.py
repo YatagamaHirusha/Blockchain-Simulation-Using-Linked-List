9e68654e6fd3606a99a14c753a5edf6566f4bfd9
hirushayatagama <hirushayatagama@gmail.com>
2026-01-02 08:16:02 +0530

    added distributed system concept

---
+++ Server.py
@@ -0,0 +1,32 @@
+from http.client import responses
+
+from flask import Flask, jsonify, request
+from Blockchain import Blockchain
+from Block import Block
+from uuid import uuid4
+
+# Instantiate our node
+app = Flask(__name__)
+
+# Instantiate the blockchain
+blockchain = Blockchain()
+
+# Get the full chain
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    # Convert Block objects to JSON
+    chain_data = []
+    for block in blockchain.chain:
+        chain_data.append({
+            'index': block.index,
+            'timestamp': block.timestamp,
+            'data': block.data,
+            'hash': block.hash,
+            'previous_hash': block.previous_hash,
+            'nonce': block.nonce
+        })
+
+    response = {
+        'chain': chain_data,
+        'length': len(blockchain.chain),
+    }
