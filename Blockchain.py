f4fbc0a131e4215e7739d3dea111a6a1925c113f
hirushayatagama <hirushayatagama@gmail.com>
2026-01-02 07:32:31 +0530

    added distributed system concept

--- Blockchain.py
+++ Blockchain.py
@@ -1,9 +1,11 @@
 from Block import Block
+import requests
 
 class Blockchain:
     def __init__(self):
         #creating a chain
         self.chain = [self.create_genesis_block()]  #this will add the returned object from below function to a list.
+        self.nodes = set()  # Stores unique node addresses in chain
 
     def create_genesis_block(self):
         return Block(0, "Genesis Block", "00000")
@@ -32,3 +34,40 @@ class Blockchain:
                 return False
 
         return True
+
+    def register_node(self, address):
+        """
+        Add a new node to a list of nodes
+        (Ip address)
+        """
+        self.nodes.add(address)
+
+    def resolve_conflicts(self):
+        """
+        This is the consensus algorithm. It resolves conflicts
+        by replacing our chain with the longest one in the chain (network)
+        If our chain replaced with the longest node, it returns true, if not, false.
+        """
+        neighbors = self.nodes
+        new_chain = None
+
+        max_length = len(self.chain)
+        for node in neighbors:
+            try:
+                response = requests.get(f"{node}/chain")
+                if response.status_code == 200:
+                    length = response.json()['length']
+                    chain_data = response.json()['chain']
+
+                    if length > max_length:
+                        max_length = length
+                        new_chain = chain_data
+
+            except:
+                continue
+
+        # if new chain found, replace existing one
+        if new_chain:
+            self.chain = new_chain
+            return True
+        return False
