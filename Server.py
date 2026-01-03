7bd2cf5e00e659900266d289539ffa9629157cd4
hirushayatagama <hirushayatagama@gmail.com>
2026-01-03 22:39:29 +0530

    added distributed system concept

--- Server.py
+++ Server.py
@@ -36,3 +36,15 @@ def full_chain():
 def register_nodes():
     values = request.get_json()
     nodes = values.get('nodes')
+
+    if nodes is None:
+        return "Error: Please supply a valid list of nodes", 400
+
+    for node in nodes:
+        blockchain.register_node(node)
+
+    response = {
+        'message': 'New nodes have been added',
+        'total_nodes': list(blockchain.nodes),
+    }
+    return jsonify(response), 201
